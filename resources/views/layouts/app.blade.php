443f4cfd0190920d56fddff14503627d76a6ccb3
vldumchev <vldumchev@masterweb.su>
2019-05-09 17:24:22 +0300

    Added app layout.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+
+  <title>Тестовое задание</title>
+</head>
+
+<body>
+  @yield('navigation')
+  @yield('content')
+
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
