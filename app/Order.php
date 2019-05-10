6ecc56966a9e438ea81a35bddddf05563a8ef713
vldumchev <vldumchev@masterweb.su>
2019-05-10 21:42:32 +0300

    Added price attribute for Order Model.

--- app/Order.php
+++ app/Order.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $appends = ['price'];
+
     public function partner()
     {
         return $this->belongsTo('App\Partner');
@@ -14,5 +16,18 @@ class Order extends Model
     public function products()
     {
         return $this->belongsToMany('App\Product', 'order_products');
+    }
+
+    public function getPriceAttribute()
+    {
+        $totalPrice = 0;
+
+        if (isset($this->products)) {
+            foreach ($this->products as $product) {
+                $totalPrice += $product->price;
+            }
+        }
+
+        return $totalPrice;
     }
 }
