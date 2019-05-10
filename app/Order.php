dd9770e159fdf2df6a032918917c4ebac222369b
vldumchev <vldumchev@masterweb.su>
2019-05-10 21:38:07 +0300

    Set relations for Order Model.

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public function partner()
+    {
+        return $this->belongsTo('App\Partner');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Product', 'order_products');
+    }
 }
