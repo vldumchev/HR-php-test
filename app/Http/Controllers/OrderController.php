21e1bcaf66bc7bb73814d35bc03c23cb0a00a61c
vldumchev <vldumchev@masterweb.su>
2019-05-12 18:44:43 +0300

    Grouped orders by categories inside Order Controller.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,12 +5,53 @@ namespace App\Http\Controllers;
 use App\Order;
 use App\Partner;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        return view('orders.list', ['orders' => Order::with('partner')->paginate(15)]);
+        $expired = Order::with('partner')
+            ->where('status', 10)
+            ->where('delivery_dt', '<', Carbon::now()->toDateTimeString())
+            ->orderBy('delivery_dt', 'desc')
+            ->limit(50)
+            ->get();
+
+        $current = Order::with('partner')
+            ->where('status', 10)
+            ->whereBetween('delivery_dt', [
+                Carbon::now()->toDateTimeString(),
+                Carbon::now()->addHours(24)->toDateTimeString()
+            ])
+            ->orderBy('delivery_dt', 'asc')
+            ->get();
+
+        $new     = Order::with('partner')
+            ->where('status', 0)
+            ->where('delivery_dt', '>', Carbon::now()->toDateTimeString())
+            ->orderBy('delivery_dt', 'asc')
+            ->limit(50)
+            ->get();
+
+        $done    = Order::with('partner')
+            ->where('status', 20)
+            ->whereBetween('delivery_dt', [
+                Carbon::now()->toDateString(),
+                Carbon::now()->addHours(24)->toDateString()
+            ])
+            ->orderBy('delivery_dt', 'desc')
+            ->limit(50)
+            ->get();
+
+        return view('orders.list', [
+            'groupedOrders' => [
+                'expired' => $expired,
+                'current' => $current,
+                'new'     => $new,
+                'done'    => $done
+            ]
+        ]);
     }
 
     public function edit($id, $errors = [])
